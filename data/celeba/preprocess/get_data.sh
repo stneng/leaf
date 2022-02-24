ef5d07f9e31d4dc029b1e8ba03b3f49f2cd5c182
stneng <git@stneng.com>
2022-02-24 18:09:36 +0800

    update celeba's download script

--- data/celeba/preprocess/get_data.sh
+++ data/celeba/preprocess/get_data.sh
@@ -10,8 +10,8 @@ cd ../data/raw
 wget 'https://drive.google.com/uc?id=1_ee_0u7vcNLOfNLegJRHmolfH5ICW-XS&export=download' -O 'identity_CelebA.txt'
 wget 'https://drive.google.com/uc?id=0B7EVK8r0v71pblRyaVFSWGxPY0U&export=download&resourcekey=0-YW2qIuRcWHy_1C2VaRGL3Q' -O 'list_attr_celeba.txt'
 wget 'https://drive.google.com/uc?id=0B7EVK8r0v71pZjFTYXZWM3FlRnM&export=download&resourcekey=0-dYn9z10tMJOBAkviAcfdyQ' --save-cookies cookies.txt --keep-session-cookies -O 'img_align_celeba.zip.token'
-token=`cat img_align_celeba.zip.token | grep -Eo "confirm=\w+&"`
-wget 'https://drive.google.com/uc?id=0B7EVK8r0v71pZjFTYXZWM3FlRnM&export=download&'$token'resourcekey=0-dYn9z10tMJOBAkviAcfdyQ' --load-cookies cookies.txt -O 'img_align_celeba.zip'
+token=`cat img_align_celeba.zip.token | grep -Eo "confirm=\w+"`
+wget 'https://drive.google.com/uc?id=0B7EVK8r0v71pZjFTYXZWM3FlRnM&export=download&'$token'&resourcekey=0-dYn9z10tMJOBAkviAcfdyQ' --load-cookies cookies.txt -O 'img_align_celeba.zip'
 rm cookies.txt img_align_celeba.zip.token
 unzip img_align_celeba.zip
 rm img_align_celeba.zip
